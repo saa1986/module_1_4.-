1a7f794840fc4022f0fed6bc2ad2a6bbdb60372b
saa1986 <1986zip@mail.ru>
2024-08-06 13:49:49 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input( "Введите фразу: ")
+print(len(my_string )) # сумма символов в веденной строке
+print(my_string.upper()) # вывод в верхнем регистре
+print(my_string.lower()) # вывод в нижнем регистре
+print(my_string.replace(" ", "#")) # изменяем строку, удаляя все пробелы
+print(my_string[0]) # вывод первого символа
+print(my_string[-1])# вывод последнего символа
